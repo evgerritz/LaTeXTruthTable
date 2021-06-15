da3b5fd755c445148f34f88f4ba6a3d09aff9333
Evan Gerritz <evgerritz@gmail.com>
2021-06-14 20:16:45 -0400

    result page fixed

--- truthtablepage/truthtable/views.py
+++ truthtablepage/truthtable/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import sympy
+import shutil
 from src import *
 
 def index(request):
@@ -8,7 +10,10 @@ def index(request):
 def result(request):
     if request.method == 'POST':
         pf = request.POST.get('pf', None)
-        result = main.maketable(pf)
-        return render(request, 'truthtable/result.html', {'pf':pf, 'result':result})
+        result_text = main.maketable(pf)
+        img_path = 'temp.png'
+        sympy.preview(result_text, viewer='file', filename=img_path, output='png')
+        shutil.move(img_path, 'truthtable/static/images/' + img_path)
+        return render(request, 'truthtable/result.html', {'pf':pf, 'result_text':result_text, 'img_path': img_path})
     else:
         return render(request, 'truthtable/index.html')
