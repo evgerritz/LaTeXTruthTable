d1be21052904749d38be378ff7e9e70edfd129c4
Evan Gerritz <evgerritz@gmail.com>
2021-06-27 15:48:14 -0400

    root url added, Procfile

--- truthtablepage/truthtablepage/urls.py
+++ truthtablepage/truthtablepage/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('truthtable.urls')),
+    path('/', include('truthtable.urls')),
     path('truthtable/', include('truthtable.urls')),
     path('admin/', admin.site.urls),
 ]
